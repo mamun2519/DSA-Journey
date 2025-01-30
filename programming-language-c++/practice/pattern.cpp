4f6e4c719c199bb41cbc85e78a7be35e1a2aed49
mamun2519 <juborajislam46@gmail.com>
2025-01-30 19:28:14 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main()
 {
-      int n = 4 for (int i = 0;)
+      int n = 4;
+      for (int i = 0; i < n; i++)
+      {
+            for (int j = 0; j < n; j++)
+            {
+            }
+      }
 }

5b412fa528958f0f2e60f1d676611846aa210f7f
mamun2519 <juborajislam46@gmail.com>
2025-01-30 21:22:54 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -6,9 +6,9 @@ int main()
       int n = 4;
       for (int i = 0; i < n; i++)
       {
-            for (j = 0; j < i + 1; j++)
+            for (int j = 1; j < i + 1; j++)
             {
-                  cout << i + 1;
+                  cout << j;
             }
             cout << endl;
       }

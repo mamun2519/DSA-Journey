ba3e41ab4dc71c672e755c4e056aa569ed97699b
mamun2519 <juborajislam46@gmail.com>
2025-01-30 19:44:46 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-      int n = n;
-      for (int i = 1; i < n; i++)
+      int n = 4;
+      for (int i = 0; i < n; i++)
       {
             char ch = 'A';
-            for (int j = 1; j <= ; j++)
+            for (int j = 0; j <= n; j++)
             {
                   cout << ch;
-                  ch = +;
+                  ch = ch + 1;
             }
             cout << endl;
       }
-      return 0
+      return 0;
 }

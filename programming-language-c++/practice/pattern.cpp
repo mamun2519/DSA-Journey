f9e8b2c659fa53a84bd2d7c532184eafce82bc17
mamun2519 <juborajislam46@gmail.com>
2025-01-30 19:37:35 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main()
 {
       int n = 4;
-      for (int i = 1; i <= n; i++)
+      for (int i = 1; i < n; i++)
       {
-            for (int j = 1; j <= n; j++)
+            for (ini j = 1; j <= n; j++)
             {
                   cout << j;
             }
-            cout << endl;
       }
-
-      return 0;
 }

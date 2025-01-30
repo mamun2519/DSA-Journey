f85809c0bf8b19a81b001aac94f9b71f2da9cc53
mamun2519 <juborajislam46@gmail.com>
2025-01-30 19:47:27 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
       int n = 4;
+      int number = 1;
       for (int i = 0; i < n; i++)
       {
-            char ch = 'A';
+
             for (int j = 0; j <= n; j++)
             {
-                  cout << ch;
-                  ch = ch + 1;
+                  cout << number << " ";
+                  number++;
             }
             cout << endl;
       }

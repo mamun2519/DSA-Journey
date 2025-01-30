ed561c78cc5e5cde5735082b60560957e4a847cb
mamun2519 <juborajislam46@gmail.com>
2025-01-30 21:47:41 +0600

    c++ practice problem solving

--- programming-language-c++/practice/pattern.cpp
+++ programming-language-c++/practice/pattern.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int sumN = (int n)
+{
+      int sum = 0;
+      for (int i = 1; i <= n; i++)
+      {
+            sum += i
+      }
+      return sum;
+}
+
 int main()
 {
-      int n = 4;
-      for (int i = 0; i < n; i++)
-      {
-            for (int j = 1; j < i + 1; j++)
-            {
-                  cout << j;
-            }
-            cout << endl;
-      }
+
+      cout >> sumN(5);
+      return 0
 }

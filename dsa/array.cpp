f85f6a7834a73609e3f6fc43d28f8cc655af9734
mamun2519 <juborajislam46@gmail.com>
2025-02-04 21:11:42 +0600

    Practice: Dsa Array practice and explore

--- dsa/array.cpp
+++ dsa/array.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-      cout << "Hello world" << endl;
+      int numbers[] = {1, 3, 3, 5, 6, 7};
+      int size = 6;
+      for (int i = 0; i < size; i++)
+      {
+            cout << numbers[i] << endl;
+      }
+
       return 0;
 }

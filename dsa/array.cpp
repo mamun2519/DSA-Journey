cd9b471ff83bdc1dbf309b06632dff1bc3089bd0
mamun2519 <juborajislam46@gmail.com>
2025-02-04 21:13:15 +0600

    Practice: Dsa Array practice and explore

--- dsa/array.cpp
+++ dsa/array.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//       int numbers[] = {1, 3, 3, 5, 6, 7};
+//       int size = 6;
+//       for (int i = 0; i < size; i++)
+//       {
+//             cout << numbers[i] << endl;
+//       }
+
+//       return 0;
+// }
+
+//& Find the largest Element in array
+
 int main()
 {
-      int numbers[] = {1, 3, 3, 5, 6, 7};
-      int size = 6;
-      for (int i = 0; i < size; i++)
-      {
-            cout << numbers[i] << endl;
-      }
 
       return 0;
 }

f9b130d02fff6ca44f17caee922b87edc773b1aa
mamun2519 <juborajislam46@gmail.com>
2025-02-09 21:29:38 +0600

    practice: Practice Verctor related problem

--- dsa/vectors.cpp
+++ dsa/vectors.cpp
@@ -6,6 +6,9 @@ int main()
 {
 
       vector<int> numbers = {1, 3, 5, 7, 8};
-      cout << numbers[3];
+      numbers.push_back(20);
+      numbers.emplace_back(30)
+              cout
+          << numbers[5];
       return 0;
 }

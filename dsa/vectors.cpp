4a96ee6bfa232936f7ce1abd887f34e8ecf2e71e
mamun2519 <juborajislam46@gmail.com>
2025-02-09 21:40:42 +0600

    practice: Practice Verctor related problem

--- dsa/vectors.cpp
+++ dsa/vectors.cpp
@@ -15,5 +15,8 @@ int main()
           << "Adding Element Is" << numbers[6] << endl;
       cout << "Access element is" << numbers.at(2) << "First element is" << numbers.front() << "Last element is" << numbers.back() << endl;
 
+      // size and capacity
+      cout << "Size is" << numbers.size() << "capacity" << numbers.capacity() << "resize" << numbers.resize(2);
+
       return 0;
 }

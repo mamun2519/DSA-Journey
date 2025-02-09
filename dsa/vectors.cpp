cff0fad9ff1fb47f82e283bd31200d64de963ede
mamun2519 <juborajislam46@gmail.com>
2025-02-09 19:46:34 +0600

    practice: Practice Verctor related problem

--- dsa/vectors.cpp
+++ dsa/vectors.cpp
@@ -6,5 +6,7 @@ int main()
 {
       vector<int> v1;
       vector<string> v2 = {"apple", "banana", "cherry"};
+      vector<double> v2(4, 5, 7);
+      vector<int> numbers = {1, 3, 5, 7, 8};
       return 0;
 }

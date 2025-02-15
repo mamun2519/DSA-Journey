7386f05a367ba07fce665dea16c3c84964ef6282
mamun2519 <juborajislam46@gmail.com>
2025-02-15 20:46:36 +0600

    Practice: Practice time space complexity

--- dsa/time&spaceCom.cpp
+++ dsa/time&spaceCom.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int findMax(int arr[], int size)
+{
+      int max = arr[0];
+      for (int i = 0; i < size; i++)
+      {
+      }
+}
+
 int main()
 {
       int arr[] = {10, 20, 30, 40, 50};
-      int size = sizeof(arr) / sizeof(arr[0])
-                                   cout
-                 << size;
+      int size = sizeof(arr) / sizeof(arr[0]);
+      cout
+          << size;
       return 0;
 }

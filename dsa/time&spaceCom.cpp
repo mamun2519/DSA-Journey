bc80750bfb52db1e9ebe835aee46203848d4b08f
mamun2519 <juborajislam46@gmail.com>
2025-02-15 20:44:51 +0600

    Practice: Practice time space complexity

--- dsa/time&spaceCom.cpp
+++ dsa/time&spaceCom.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
-      cout << "Hello world";
+      int arr[] = {10, 20, 30, 40, 50};
+      int size = sizeof(arr) / sizeof(arr[0])
+                                   cout
+                 << size;
       return 0;
 }

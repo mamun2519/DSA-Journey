1017718c7ab9767d91d0b767a00fc86f3cf6cf98
mamun2519 <juborajislam46@gmail.com>
2025-02-23 21:17:46 +0600

    practice: Practice binary search algo

--- dsa/binary-search.cpp
+++ dsa/binary-search.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int binarySearch(int arr[], int size, int target)
 {
       int left = 0;
-      int right = size - 1
+      int right = size - 1;
+      while (left <= right)
+      {
+            /* code */
+      }
 }

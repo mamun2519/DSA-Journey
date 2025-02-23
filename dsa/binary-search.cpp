5205bdc0c1e1f2de8c0f93940699373315d083c8
mamun2519 <juborajislam46@gmail.com>
2025-02-23 21:23:51 +0600

    practice: Practice binary search algo

--- dsa/binary-search.cpp
+++ dsa/binary-search.cpp
@@ -25,3 +25,21 @@ int binarySearch(int arr[], int size, int target)
 
       return -1;
 }
+
+int main()
+{
+      int arr[] = {10, 20, 30, 40, 50};
+      int size = sizeof(arr) / sizeof(arr[0]);
+      int target = 30;
+
+      int result = binarySearch(arr, size, target);
+      if (result != -1)
+      {
+            cout << "Element found at index: " << result << endl;
+      }
+      else
+      {
+            cout << "Element not found in the array." << endl;
+      }
+      return 0;
+}

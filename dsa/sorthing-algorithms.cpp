ecaaf219afc9949b64b8843dd111c648af5f7a1e
mamun2519 <juborajislam46@gmail.com>
2025-03-06 23:54:56 +0600

    practice: sorting algothom practice

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -28,6 +28,7 @@ void selectionSort(int arr[], int size)
                         smallIndex = j;
                   }
             }
+            swap(arr[i], arr[smallIndex]);
       }
 }
 
@@ -43,7 +44,8 @@ int main()
 {
       int size = 5;
       int arr[] = {4, 1, 5, 2, 3};
-      bubbleSort(arr, size);
+      // bubbleSort(arr, size);
+      selectionSort(arr, size);
       printArray(arr, size);
       return 0;
 }

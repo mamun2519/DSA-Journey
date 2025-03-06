1dd7c832f696078145b6c1d7c67eead4ff9c8d05
mamun2519 <juborajislam46@gmail.com>
2025-03-06 23:53:19 +0600

    practice: sorting algothom practice

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -20,6 +20,14 @@ void selectionSort(int arr[], int size)
 {
       for (int i = 0; i < size - 1; i++)
       {
+            int smallIndex = i;
+            for (int j = i + 1; j < size; j++)
+            {
+                  if (arr[j] < arr[smallIndex])
+                  {
+                        smallIndex = j;
+                  }
+            }
       }
 }
 

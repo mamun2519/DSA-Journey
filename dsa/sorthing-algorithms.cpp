cf04a18ec07f6393fd525d9ebd348b92b197abd7
mamun2519 <juborajislam46@gmail.com>
2025-03-07 16:21:41 +0600

    practice bubbole sort

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -38,6 +38,11 @@ void insertionSort(int arr[], int size)
       for (i = 1; i < size; i++)
       {
             int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 * *arr[j] > key)
+            {
+                  /* code */
+            }
       }
 }
 

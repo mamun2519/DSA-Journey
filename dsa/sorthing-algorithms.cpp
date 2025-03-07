ed1672d7623c8ed72c19a03a4a4261f0234f1da6
mamun2519 <juborajislam46@gmail.com>
2025-03-07 16:24:37 +0600

    practice bubbole sort

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -39,9 +39,9 @@ void insertionSort(int arr[], int size)
       {
             int curr = arr[i];
             int prev = i - 1;
-            while (prev >= 0 && arr[j] > curr)
+            while (prev >= 0 && arr[prev] > curr)
             {
-                  arr[j + 1] = arr[prev];
+                  arr[prev + 1] = arr[prev];
                   prev--;
             }
             arr[prev + 1] = curr;

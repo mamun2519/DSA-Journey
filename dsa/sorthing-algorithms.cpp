70b87c49d1fd226f65f795b9018830314d2f5779
mamun2519 <juborajislam46@gmail.com>
2025-03-07 16:23:29 +0600

    practice bubbole sort

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -35,11 +35,11 @@ using namespace std;
 
 void insertionSort(int arr[], int size)
 {
-      for (i = 1; i < size; i++)
+      for (int i = 1; i < size; i++)
       {
             int key = arr[i];
             int j = i - 1;
-            while (j >= 0 * *arr[j] > key)
+            while (j >= 0 && arr[j] > key)
             {
                   arr[j + 1] = arr[j];
                   j--;

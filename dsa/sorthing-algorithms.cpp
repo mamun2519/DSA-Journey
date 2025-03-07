302933055c0971f7c9a42101d9b039f28bda8718
mamun2519 <juborajislam46@gmail.com>
2025-03-07 15:43:54 +0600

    practice bubbole sort

--- dsa/sorthing-algorithms.cpp
+++ dsa/sorthing-algorithms.cpp
@@ -36,7 +36,13 @@ void bubbleSort(int arr[], int size)
 {
       for (int i = 0; i < size - 1; i++)
       {
-            for
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                  if (arr[i] > arr[j + 1])
+                  {
+                        swap(arr[j], arr[j + 1]);
+                  }
+            }
       }
 }
 

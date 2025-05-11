40e6ce27f9e29265145d4aee4b70b1f08022007f
mamun2519 <juborajislam46@gmail.com>
2025-05-11 14:31:30 +0600

    try to solve problem find duplicate array using bynary search

--- DSA/problem_01.cpp
+++ DSA/problem_01.cpp
@@ -1,65 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// bool binarySearch(int array[], int searchTerm, int n)
-// {
-
-//       int flag = false;
-//       int left_side = 0;
-//       int right_side = n - 1;
-
-//       while (left_side <= right_side)
-//       {
-//             int mid_index = (left_side + right_side) / 2;
-
-//             if (array[mid_index] == searchTerm)
-//             {
-
-//                   if (array[mid_index - 1] == searchTerm || array[mid_index + 1] == searchTerm)
-//                   {
-//                         return true;
-//                   }
-//                   else
-//                   {
-//                         return false;
-//                   }
-//             }
-//             else if (searchTerm > array[mid_index])
-//             {
-//                   left_side = mid_index + 1;
-//             }
-//             else
-//             {
-//                   right_side = mid_index - 1;
-//             }
-//       }
-
-//       return flag;
-// };
-
-// int main()
-// {
-
-//       int n;
-//       cin >> n;
-//       int array[n];
-//       for (int i = 0; i < n; i++)
-//       {
-//             cin >> array[i];
-//       }
-//       int searchTerm;
-//       cin >> searchTerm;
-//       int isDuplicate = binarySearch(array, searchTerm, n);
-//       cout << isDuplicate << endl;
-//       if (isDuplicate)
-//             cout << "true";
-//       else
-//             cout << "false";
-
-//       return 0;
-// }
-
-// find duplicate value using bainary search
+// find duplicate value using binary search
 bool findIsDuplicate(int array[], int n, int searchTerm)
 {
       int left_side = 0;
@@ -71,6 +13,14 @@ bool findIsDuplicate(int array[], int n, int searchTerm)
 
             if (searchTerm == array[middle_index])
             {
+                  if (array[middle_index - 1] == searchTerm || array[middle_index + 1] == searchTerm)
+                  {
+                        return true;
+                  }
+                  else
+                  {
+                        return false;
+                  }
             }
 
             else if (array[middle_index] < searchTerm)
@@ -97,5 +47,9 @@ int main()
       cin >> searchTerm;
 
       bool isDuplicate = findIsDuplicate(array, n, searchTerm);
+      if (isDuplicate)
+            cout << "true";
+      else
+            cout << "false";
       return 0;
 }

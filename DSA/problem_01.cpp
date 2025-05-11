5510344a299533fe1169c3099fa6830b2b2cc0eb
mamun2519 <juborajislam46@gmail.com>
2025-05-11 13:00:31 +0600

    added to optmize bynari search

---
+++ DSA/problem_01.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(int array[], int searchTerm, int n)
+{
+
+      int dublicate = 0;
+      int left_side = 0;
+      int right_side = n - 1;
+
+      while (left_side <= right_side)
+      {
+            int mid_index = (left_side + right_side) / 2;
+
+            if (array[mid_index] == searchTerm)
+            {
+                  dublicate = dublicate + 1;
+            }
+            if (searchTerm > array[mid_index])
+            {
+                  left_side = mid_index + 1;
+            }
+            else
+            {
+                  right_side = mid_index - 1;
+            }
+      }
+
+      return flag;
+};
+
+int main()
+{
+
+      int n;
+      cin >> n;
+      int array[n];
+      for (int i = 0; i < n; i++)
+      {
+            cin >> array[i];
+      }
+      int searchTerm;
+      cin >> searchTerm;
+      int flag = binarySearch(array, searchTerm, n);
+
+      if (flag == true)
+            cout << "SearchTerm Find Successfully!";
+      else
+            cout << "Opps! SearchTerm Does not find!";
+
+      return 0;
+}

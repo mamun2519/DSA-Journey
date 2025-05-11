bb80c8e880cae917dd3e5a2febd47607f847d196
mamun2519 <juborajislam46@gmail.com>
2025-05-11 14:18:59 +0600

    try to solve problem find duplicate array using bynary search

--- DSA/problem_01.cpp
+++ DSA/problem_01.cpp
@@ -63,6 +63,12 @@ using namespace std;
 
 int main()
 {
-
+      int n;
+      cin >> n;
+      int array[n];
+      for (int i = 0; i < n; i++)
+      {
+            cin >> array[i];
+      }
       return 0;
 }

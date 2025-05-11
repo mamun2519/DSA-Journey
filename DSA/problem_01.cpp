e00e597a99c9ebe8223850255a460164afb66362
mamun2519 <juborajislam46@gmail.com>
2025-05-11 14:17:57 +0600

    try to solve problem find duplicate array using bynary search

--- DSA/problem_01.cpp
+++ DSA/problem_01.cpp
@@ -1,60 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool binarySearch(int array[], int searchTerm, int n)
-{
+// bool binarySearch(int array[], int searchTerm, int n)
+// {
 
-      int flag = false;
-      int left_side = 0;
-      int right_side = n - 1;
+//       int flag = false;
+//       int left_side = 0;
+//       int right_side = n - 1;
 
-      while (left_side <= right_side)
-      {
-            int mid_index = (left_side + right_side) / 2;
+//       while (left_side <= right_side)
+//       {
+//             int mid_index = (left_side + right_side) / 2;
 
-            if (array[mid_index] == searchTerm)
-            {
+//             if (array[mid_index] == searchTerm)
+//             {
 
-                  if (array[mid_index - 1] == searchTerm || array[mid_index + 1] == searchTerm)
-                  {
-                        return true;
-                  }
-                  else
-                  {
-                        return false;
-                  }
-            }
-            else if (searchTerm > array[mid_index])
-            {
-                  left_side = mid_index + 1;
-            }
-            else
-            {
-                  right_side = mid_index - 1;
-            }
-      }
+//                   if (array[mid_index - 1] == searchTerm || array[mid_index + 1] == searchTerm)
+//                   {
+//                         return true;
+//                   }
+//                   else
+//                   {
+//                         return false;
+//                   }
+//             }
+//             else if (searchTerm > array[mid_index])
+//             {
+//                   left_side = mid_index + 1;
+//             }
+//             else
+//             {
+//                   right_side = mid_index - 1;
+//             }
+//       }
 
-      return flag;
-};
+//       return flag;
+// };
+
+// int main()
+// {
+
+//       int n;
+//       cin >> n;
+//       int array[n];
+//       for (int i = 0; i < n; i++)
+//       {
+//             cin >> array[i];
+//       }
+//       int searchTerm;
+//       cin >> searchTerm;
+//       int isDuplicate = binarySearch(array, searchTerm, n);
+//       cout << isDuplicate << endl;
+//       if (isDuplicate)
+//             cout << "true";
+//       else
+//             cout << "false";
+
+//       return 0;
+// }
+
+// find duplicate value using bainary search
 
 int main()
 {
-
-      int n;
-      cin >> n;
-      int array[n];
-      for (int i = 0; i < n; i++)
-      {
-            cin >> array[i];
-      }
-      int searchTerm;
-      cin >> searchTerm;
-      int isDuplicate = binarySearch(array, searchTerm, n);
-      cout << isDuplicate << endl;
-      if (isDuplicate)
-            cout << "true";
-      else
-            cout << "false";
 
       return 0;
 }

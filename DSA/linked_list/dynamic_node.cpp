b4ce7f5d2c2af3dde7ac77aeb395b2aa6e9d671e
mamun2519 <juborajislam46@gmail.com>
2025-05-13 12:33:00 +0600

    added to dynamic node

--- DSA/linked_list/dynamic_node.cpp
+++ DSA/linked_list/dynamic_node.cpp
@@ -18,8 +18,10 @@ int main()
 {
       Node *Head = new Node(10);
       Node *a = new Node(5);
+      Node *b = new Node(4);
 
       Head->next = a;
+      a->next = b;
 
       cout << Head->value << " " << Head->next->value;
       return 0;

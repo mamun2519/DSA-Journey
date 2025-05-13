4c162a97277bbc1e79dda62ce7b51109f2eaba47
mamun2519 <juborajislam46@gmail.com>
2025-05-13 12:13:03 +0600

    added to singly linked list

--- DSA/linked_list/singly.link_list.cpp
+++ DSA/linked_list/singly.link_list.cpp
@@ -1,17 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Node
 {
 public:
       int value;
       Node *next;
-
-      Node(int val)
-      {
-            this->value = val;
-            this->next = NULL;
-      }
 };
 int main()
 {

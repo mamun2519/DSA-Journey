bbfd6e2c01d072ffb8c1dea275a163895190ef83
mamun2519 <juborajislam46@gmail.com>
2025-06-21 20:18:20 +0600

    added to reverse array

--- dsa/Array-Operations/reverse.cpp
+++ dsa/Array-Operations/reverse.cpp
@@ -8,5 +8,13 @@ int main()
       int arr[n];
       for (int i = 0; i < n; i++)
             cin >> arr[i];
+
+      // reverse the array
+      for (int i = 0; i < n / 2; i++)
+      {
+            swap(arr[i], arr[n - i - 1]);
+      }
+      for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
       return 0;
 }

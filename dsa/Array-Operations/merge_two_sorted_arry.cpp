2522c626858366705c63b8bb7c583e963838c0ce
mamun2519 <juborajislam46@gmail.com>
2025-06-21 22:15:10 +0600

    added to merge 2 array in the sorted

--- dsa/Array-Operations/merge_two_sorted_arry.cpp
+++ dsa/Array-Operations/merge_two_sorted_arry.cpp
@@ -1,2 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main()
+{
+      int n;
+      cin >> n;
+      int a[n];
+      for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+      int m;
+      cin >> m;
+      int b[m];
+      for (int i = 0; i < m; i++)
+            cin >> b[i];
+
+      int c[n + m];
+      return 0;
+}

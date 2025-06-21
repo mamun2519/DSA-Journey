319883bf8f9c6bffbd043d0e6497f66e61640aa9
mamun2519 <juborajislam46@gmail.com>
2025-06-21 22:24:14 +0600

    check the sorted arry

---
+++ dsa/Array-Operations/check_sorted.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n;
+      cin >> n;
+      int arr[n];
+
+      for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+      // check if the array is sorted
+      bool isSorted = true;
+      for (int i = 0; i < n; i++)
+      {
+            if (arr[i] > arr[i + 1])
+            {
+                  isSorted = false;
+                  break;
+            }
+      }
+      return 0;
+}

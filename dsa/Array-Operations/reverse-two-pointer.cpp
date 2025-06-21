c54e7bab7804e1b1bdcbd2e904d892bec1d465cd
mamun2519 <juborajislam46@gmail.com>
2025-06-21 21:40:22 +0600

    added to two pointer reverse arry

--- dsa/Array-Operations/reverse-two-pointer.cpp
+++ dsa/Array-Operations/reverse-two-pointer.cpp
@@ -9,4 +9,13 @@ int main()
       for (int i = 0; i < n; i++)
             cin >> arr[i];
       // reverse the array using two pointer approach
+
+      int left = 0, right = n - 1;
+
+      while (left < right)
+      {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+      }
 }

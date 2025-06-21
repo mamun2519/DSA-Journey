1ee3fd9fe254d14eefc6804d29902a00279ad926
mamun2519 <juborajislam46@gmail.com>
2025-06-21 21:48:54 +0600

    add to merge arry

--- dsa/Array-Operations/merge_arry.cpp
+++ dsa/Array-Operations/merge_arry.cpp
@@ -5,6 +5,13 @@ int main()
 {
       int n;
       cin >> n;
+      int arr[n];
       for (int i = 0; i < n; i++)
             cin >> arr[i];
+
+      int m;
+      cin >> m;
+      int arr2[m];
+      for (int i = 0; i < m; i++)
+            cin >> arr2[i];
 }

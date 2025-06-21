80d4b8687b8fe5868098a7df711407c3d0389792
mamun2519 <juborajislam46@gmail.com>
2025-06-21 18:00:07 +0600

    added to insert array operation

--- dsa/Array-Operations/insert_array.cpp
+++ dsa/Array-Operations/insert_array.cpp
@@ -6,7 +6,12 @@ int main()
       int n;
       cin >> n;
       int arr[n + 1];
+      for (int i = 0; i < n; i++)
+            cin >> arr[i];
       // recive index and value to be inserted
       int index, value;
+      cin >> index >> value;
+
+      // pint the array
       return 0;
 }

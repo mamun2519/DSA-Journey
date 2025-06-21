c34f18c865063c6eb5ccaa038223a26877e82ad3
mamun2519 <juborajislam46@gmail.com>
2025-06-21 18:38:41 +0600

    added to array deletion

--- dsa/Array-Operations/deletion.cpp
+++ dsa/Array-Operations/deletion.cpp
@@ -6,5 +6,11 @@ int main()
       int n;
       cin >> n;
       int arr[n];
+      // take input for the array
+      for (int i = 0; i < n; i++)
+            cin >> arr[i];
+      // receive index
+      int index;
+      cin >> index;
       return 0;
 }

b14b6dc8951928a39cb55ac3ea62dcabd3d489c9
mamun2519 <juborajislam46@gmail.com>
2025-06-22 19:59:11 +0600

    added to reverse arry problem

--- practice/array_reverse.cpp
+++ practice/array_reverse.cpp
@@ -8,7 +8,20 @@ int main()
       int arr[n];
       for (int i = 0; i < n; i++)
       {
-            cin >> arr[n];
+            cin >> arr[i];
+      }
+      // reverse the array
+
+      for (int i = 0; i < n / 2; i++)
+      {
+            cout << "N =>" << n << endl;
+            swap(arr[i], arr[n - i - 1]);
+      }
+
+      // pint
+      for (int i = 0; i < n; i++)
+      {
+            cout << arr[i] << " ";
       }
       return 0;
 }

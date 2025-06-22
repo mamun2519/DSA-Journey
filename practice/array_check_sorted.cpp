8b27c28c7f1c7f7951b8dbe839d55289308536f0
mamun2519 <juborajislam46@gmail.com>
2025-06-22 23:13:36 +0600

    added to merge sorted array

---
+++ practice/array_check_sorted.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n;
+      cin >> n;
+      int a[n];
+      for (int i = 0; i < n; i++)
+            cin >> a[i];
+      // check the sorted array
+      bool isSorted = false;
+      for (int i = 0; i < n; i++)
+      {
+
+            if (a[i] > a[i + 1])
+            {
+                  isSorted = false;
+                  break;
+            }
+      }
+      if (isSorted)
+            cout << "Array is sorted" << endl;
+      else
+            cout << "Array is not sorted" << endl;
+      return 0;
+}

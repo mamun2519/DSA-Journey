b4f157226e272843aef3a8d416436e6c8adf5391
mamun2519 <juborajislam46@gmail.com>
2025-06-22 19:47:38 +0600

    practice array operation

--- practice/array_deletion.cpp
+++ practice/array_deletion.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
-
+      int n;
+      cin >> n;
+      int arr[n];
+      for (int i = 0; i < n; i++)
+            cin >> arr[i];
+      int index;
+      cin >> index;
+      for (int i = index + 1; i < n; i++)
+      {
+            arr[i - 1] = arr[i];
+      }
+      // pint
+      for (int i = 0; i < n - 1; i++)
+      {
+            cout << arr[i] << " ";
+      }
       return 0;
 }

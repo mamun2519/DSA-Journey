2bc55fce11d5360950c265895c3bf3b7d078d69a
mamun2519 <juborajislam46@gmail.com>
2025-06-22 18:16:08 +0600

    practice array operation

--- practice/array_insert.cpp
+++ practice/array_insert.cpp
@@ -4,5 +4,16 @@ int main()
 {
       int n;
       cin >> n;
+      int arr[n + 1];
+      // receive the array
+      for (int i = 0; i < n; i++)
+      {
+            cin >> arr[i];
+      }
+      for (int i = 0; i < n; i++)
+      {
+            cout << arr[i] << " " << endl;
+      }
+      // pint the array
       return 0;
 }

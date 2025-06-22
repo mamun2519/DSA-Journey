206a74c9c55d4d838985aa31660d71f634f24119
mamun2519 <juborajislam46@gmail.com>
2025-06-22 18:33:09 +0600

    practice array operation

--- dsa/Array-Operations/insert_array.cpp
+++ dsa/Array-Operations/insert_array.cpp
@@ -8,20 +8,10 @@ int main()
       cin >> n;
       int arr[n + 1];
       for (int i = 0; i < n; i++)
+      {
             cin >> arr[i];
-      // recive index and value to be inserted
+      }
       int index, value;
-      cin >> index >> value;
-      // shift elements to the right
-      for (int i = n - 1; i >= index; i--)
-      {
-            arr[i + 1] = arr[i];
-      }
-      arr[index] = value; // insert the value at the index
-      // pint the array
-      for (int i = 0; i <= n; i++)
-      {
-            cout << arr[i] << " ";
-      }
+      cin >> index, value;
       return 0;
 }

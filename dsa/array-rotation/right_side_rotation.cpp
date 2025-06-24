d50957c32cb383ff770ea508259029e4d174f333
mamun2519 <juborajislam46@gmail.com>
2025-06-24 12:02:49 +0600

    added to right side one time array rotation problem

--- dsa/array-rotation/right_side_rotation.cpp
+++ dsa/array-rotation/right_side_rotation.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int main()
 {
 
+      int n;
+      cin >> n;
+      int arr[n];
+      for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+      // right side one time rotated
+      int lastIndex = arr[n - 1];
+      cout << "last index" << arr[n - 1];
       return 0;
 }

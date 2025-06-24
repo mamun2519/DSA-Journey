0ff358d0ff2a0a78e988c42590faa4363ed2cf30
mamun2519 <juborajislam46@gmail.com>
2025-06-24 11:47:34 +0600

    added to one time array lolation

--- dsa/array-rotation/left_side_array_rotaion.cpp
+++ dsa/array-rotation/left_side_array_rotaion.cpp
@@ -12,13 +12,13 @@ int main()
 
       // rotated the array--------
       // store the first index
-      int firstIndex = arr[0];
+
+      int firstValue = arr[0];
       for (int i = 0; i < n - 1; i++)
       {
             arr[i] = arr[i + 1];
       }
-      arr[n - 1] = firstIndex;
-
+      arr[n - 1] = firstValue;
       // pint the array
       for (int i = 0; i < n; i++)
       {

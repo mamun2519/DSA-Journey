94325bfe71acc787c9c73d4ca02e4414782a002f
mamun2519 <juborajislam46@gmail.com>
2025-06-25 16:05:05 +0600

    added to dynamic memory allocation

--- dsa/memory_allocation/index.cpp
+++ dsa/memory_allocation/index.cpp
@@ -4,5 +4,23 @@ using namespace std;
 int main()
 {
 
+      // allocate the single integer
+      int *prt = new int;
+      *prt = 10;
+      cout << *prt;
+
+      // allocate array dynamically
+
+      int *student = new int[4];
+
+      for (int i = 0; i < 5; i++)
+            student[i] = i + 1;
+
+      // pint the array
+      for (int i = 0; i < 4; i++)
+      {
+            cout << student[i] << " ";
+      }
+
       return 0;
 }

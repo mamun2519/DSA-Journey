6f93233b7822d292fea02348d7f33a30f68f59a1
mamun2519 <juborajislam46@gmail.com>
2025-06-25 13:06:18 +0600

    added to left side roation k time bed case

--- dsa/array-rotation/left_site_k_time_reverse_good.cpp
+++ dsa/array-rotation/left_site_k_time_reverse_good.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void reverse(int *a, int i, int j)
+{
+      while (i < j)
+      {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+            /* code */
+      }
+}
 int main()
 {
       int n;
@@ -8,5 +18,8 @@ int main()
       int a[n];
       for (int i = 0; i < n; i++)
             cin >> a[i];
+
+      int k;
+      cin >> k;
       return 0;
 }

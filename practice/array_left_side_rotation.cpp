450319919d01294ef4cc8cd9e21d76d6d482a022
mamun2519 <juborajislam46@gmail.com>
2025-06-25 15:13:34 +0600

    added to right side k time bed case array rotation

--- practice/array_left_side_rotation.cpp
+++ practice/array_left_side_rotation.cpp
@@ -11,16 +11,9 @@ int main()
             cin >> a[i];
 
       int firstIndex = a[0];
-
-      for (int i = 0; i < n; i++)
-      {
-            a[i - 1] = a[i];
-      }
-      a[n - 1] = firstIndex;
-
       for (int i = 0; i <= n; i++)
       {
-            cout << arr[i] << " ";
+            cout << a[i] << " ";
       }
       return 0;
 }

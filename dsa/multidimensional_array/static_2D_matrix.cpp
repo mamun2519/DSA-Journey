af71222633a5f659553ab2827fa63ee8b7e5de0e
mamun2519 <juborajislam46@gmail.com>
2025-06-26 15:30:53 +0600

    added to static 2d matrix

--- dsa/multidimensional_array/static_2D_matrix.cpp
+++ dsa/multidimensional_array/static_2D_matrix.cpp
@@ -6,5 +6,15 @@ int main()
 { // two row => 3 column
       int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
+      // travarse array
+
+      for (int i = 0; i < 2; i++)
+      {
+            for (int j = 0; j < 4; j++)
+            {
+                  cout << matrix[i] << " ";
+            }
+      }
+
       return 0;
 }

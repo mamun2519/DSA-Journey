ed0edc2774aed7e36cde466e373e8decef693d4d
mamun2519 <juborajislam46@gmail.com>
2025-06-26 15:15:27 +0600

    added to static 2d matrix

--- dsa/multidimensional_array/static_2D_matrix.cpp
+++ dsa/multidimensional_array/static_2D_matrix.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // g++ insert_array.cpp -o test.exe && ./test.exe < input.txt > output.txt
 
 int main()
-{
+{ // two row => 3 column
       int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
       return 0;

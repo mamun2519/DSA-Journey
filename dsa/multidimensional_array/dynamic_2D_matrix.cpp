e3619bcae602e923806bad7c39e1d0de74d8064d
mamun2519 <juborajislam46@gmail.com>
2025-06-26 15:50:28 +0600

    added to dynamic 2D martrix

--- dsa/multidimensional_array/dynamic_2D_matrix.cpp
+++ dsa/multidimensional_array/dynamic_2D_matrix.cpp
@@ -25,5 +25,13 @@ int main()
             }
       }
 
+      // pint the matrix
+      for (int i = 0; i < rows; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+            }
+      }
+
       return 0;
 }

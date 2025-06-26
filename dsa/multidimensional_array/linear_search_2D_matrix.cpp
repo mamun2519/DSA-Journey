8e3e20ccf7d570fc31abb8afc0a5baca462aa7b2
mamun2519 <juborajislam46@gmail.com>
2025-06-26 16:27:56 +0600

    added to linear search in the 2d martrix

--- dsa/multidimensional_array/linear_search_2D_matrix.cpp
+++ dsa/multidimensional_array/linear_search_2D_matrix.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main()
 {
       int matrix[3][4] = {{2, 4, 6, 8}, {5, 1, 3, 7}, {20, 50, 90, 80}};
+      int target;
+      cin >> target;
+      for (int i = 0; i < 3; i++)
+      {
+            for (int j = 0; j < 4; j++)
+            {
+            }
+      }
       return 0;
 }

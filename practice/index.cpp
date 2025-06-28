8cdc7512ac23d161fd183aae7c770ca8db0eceb5
mamun2519 <juborajislam46@gmail.com>
2025-06-28 16:36:03 +0600

    added to array inseration pratice

--- practice/index.cpp
+++ practice/index.cpp
@@ -14,6 +14,11 @@ int main()
       int index;
       cin >> index;
 
+      for (int i = index + 1; i < n; i++)
+      {
+            arr[i] = arr[i - 1];
+      }
+
       for (int i = 0; i < n; i++)
       {
             cout << arr[i] << " ";

06a9fb04622a2255eadb9cc685f3a77c61c2dbb3
mamun2519 <juborajislam46@gmail.com>
2025-06-28 16:38:46 +0600

    added to array inseration pratice

--- practice/index.cpp
+++ practice/index.cpp
@@ -6,23 +6,23 @@ int main()
       int n;
       cin >> n;
       int arr[n];
-      // take input for the array
+
       for (int i = 0; i < n; i++)
+      {
             cin >> arr[i];
-      // receive index
+      }
       int index;
       cin >> index;
 
-      // shift elements to the left
       for (int i = index + 1; i < n; i++)
       {
             arr[i - 1] = arr[i];
       }
 
-      // pint the array after deletion
       for (int i = 0; i < n - 1; i++)
       {
             cout << arr[i] << " ";
       }
+
       return 0;
 }

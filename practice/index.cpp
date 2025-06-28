28e2416116b913e0f6c236717fd9309346971df0
mamun2519 <juborajislam46@gmail.com>
2025-06-28 16:25:03 +0600

    added to array inseration pratice

--- practice/index.cpp
+++ practice/index.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
+      int n;
+      cin >> n;
+      int arr[n + 1];
+
+      for (int i = 0; i < n; i++)
+      {
+            cin >> arr[i];
+      }
+      int index, value;
+      cin >> index >> value;
 
       return 0;
 }

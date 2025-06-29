836f8b75545cd0c4d393b609ab07c7f299ab8b28
mamun2519 <juborajislam46@gmail.com>
2025-06-29 14:26:59 +0600

    added to 2d matrix problem and concept

--- dsa/multidimensional_array/declare_2d_matrix.cpp
+++ dsa/multidimensional_array/declare_2d_matrix.cpp
@@ -19,7 +19,13 @@ int main()
 
       // pint the matrix
       for (int i = 0; i < n; i++)
+      {
             for (int j = 0; j < m; j++)
+            {
                   cout << arr[i][j] << " ";
+            }
+            cout << endl;
+      }
+
       return 0;
 }

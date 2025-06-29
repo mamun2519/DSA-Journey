e14870cfa1dd728ed164914b815655794302227e
mamun2519 <juborajislam46@gmail.com>
2025-06-29 16:00:20 +0600

    added to secendary draigram

--- dsa/multidimensional_array/secondary_diagonal.cpp
+++ dsa/multidimensional_array/secondary_diagonal.cpp
@@ -1,2 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main()
+{
+      int row, cols;
+      cin >> row >> cols;
+      int arr[row][cols];
+      for (int i = 0; i < row; i++)
+            for (int j = 0; j < cols; j++)
+                  cin >> arr[i][j];
+
+      return 0;
+}

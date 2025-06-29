c59412d1cc66a8b613fafe2d1cbb8a16aa2c535e
mamun2519 <juborajislam46@gmail.com>
2025-06-29 15:51:19 +0600

    added to primary doigonal concept and problem

--- dsa/multidimensional_array/primary_digonal.cpp
+++ dsa/multidimensional_array/primary_digonal.cpp
@@ -3,4 +3,16 @@ using namespace std;
 
 void main()
 {
+      int row, cols;
+      cin >> row >> cols;
+      int arr[row][cols];
+      for (int i = 0; i < row; i++)
+            for (int j = 0; j < cols; j++)
+                  cin >> arr[i][j];
+
+      // calculate primary daigonal
+
+      for (int i = 0; i < row; i++)
+      {
+      }
 }

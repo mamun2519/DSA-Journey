9994e1fbb5b84244d6e1821d3987f449369617d4
mamun2519 <juborajislam46@gmail.com>
2025-06-30 14:56:58 +0600

    added to search in maxtrix problem

--- dsa/multidimensional_array/search_in_matrix.cpp
+++ dsa/multidimensional_array/search_in_matrix.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-
-    return 0}
+int main()
+{
+      int row, cols;
+      cin >> row >> cols;
+      int a[row][cols];
+      for (int i = 0; i < row; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+                  cin >> a[i][j];
+            }
+      }
+      return 0;
+}

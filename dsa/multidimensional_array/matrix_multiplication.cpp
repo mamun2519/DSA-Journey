dc3f1cc6134fa5b2b505a4705ce855a6feab5d15
mamun2519 <juborajislam46@gmail.com>
2025-06-30 14:40:41 +0600

    added to maxtix multiplcaiton problem and concept

--- dsa/multidimensional_array/matrix_multiplication.cpp
+++ dsa/multidimensional_array/matrix_multiplication.cpp
@@ -5,5 +5,13 @@ int main()
 {
       int m, n, p;
       cin >> m >> n;
+      int a[m][n];
+      for (int i = 0; i < m; i++)
+      {
+            for (int j = 0; j < n; j++)
+            {
+                  cin >> a[i][j];
+            }
+      }
       return 0;
 }

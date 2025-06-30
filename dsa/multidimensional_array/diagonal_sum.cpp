b701ff894048133f0ff5ff67f04bbe1cc0b0480c
mamun2519 <juborajislam46@gmail.com>
2025-06-30 15:51:03 +0600

    added to digonal sum

--- dsa/multidimensional_array/diagonal_sum.cpp
+++ dsa/multidimensional_array/diagonal_sum.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-      int n, m;
-      cin >> n >> m;
-      int a[n][m];
+      int n;
+      cin >> n;
+      int a[n][n];
       for (int i = 0; i < n; i++)
       {
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < n; j++)
             {
                   cin >> a[i][j];
             }

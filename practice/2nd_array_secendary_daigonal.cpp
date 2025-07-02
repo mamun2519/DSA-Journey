c7d3b97b00e92965d7dddc8ae7bd9f11f4ef0086
mamun2519 <juborajislam46@gmail.com>
2025-07-02 16:14:28 +0600

    pratice 2d array

---
+++ practice/2nd_array_secendary_daigonal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int row, cols;
+      cin >> row >> cols;
+
+      int a[row][cols];
+
+      for (int i = 0; i < row; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+                  cin >> a[i][j];
+            }
+      }
+
+      for (int i = 0; i < row; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+
+                  if (i == j)
+                  {
+                        cout << a[i][j] << " ";
+                  }
+            }
+      }
+
+      return 0;
+}

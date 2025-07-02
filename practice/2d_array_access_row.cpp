fc8142d6ce98a8bb6641f4c44add1be3d4ad9260
mamun2519 <juborajislam46@gmail.com>
2025-07-02 16:02:02 +0600

    pratice 2d array

--- practice/2d_array_access_row.cpp
+++ practice/2d_array_access_row.cpp
@@ -6,5 +6,14 @@ int main()
       int row, cols;
       cin >> row >> cols;
 
+      int a[row][cols];
+
+      for (int i = 0; i < row; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+            }
+      }
+
       return 0;
 }

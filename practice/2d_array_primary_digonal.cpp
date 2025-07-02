ae99bd6707df229c103598a77b157cdacb7d63aa
mamun2519 <juborajislam46@gmail.com>
2025-07-02 16:10:10 +0600

    pratice 2d array

---
+++ practice/2d_array_primary_digonal.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int row, cols;
+      cin >> row >> cols;
+
+      int a[row][cols];
+
+      for (int i = 0; i < row; i++)
+      {
+            for (int j = 0; j < cols; j++)
+            {
+                  cin >> a[i][j];
+            }
+      }
+      int index;
+      cin >> index;
+      for (int i = 0; i < cols; i++)
+      {
+            cout << a[index][i] << " ";
+      }
+
+      return 0;
+}

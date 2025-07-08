60e1d2bf3dabcb033de9808fa12fdb839f7b43c8
mamun2519 <juborajislam46@gmail.com>
2025-07-08 11:20:51 +0600

    added to victor related operation

--- dsa/vector/index.cpp
+++ dsa/vector/index.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
+      int n;
+      vector<int> v;
+      for (int i = 0; i < n; i++)
+      {
+            int x;
+            cin >> x;
+            v.push_back(x);
+      }
 
+      // pint the victor
+      for (int i = 0; i < v.size(); i++)
+      {
+            cout << v[i];
+      }
       return 0;
 }

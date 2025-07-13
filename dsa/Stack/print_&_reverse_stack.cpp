11019db8acfc139291763a0145768c0380c31d94
mamun2519 <juborajislam46@gmail.com>
2025-07-13 14:32:51 +0600

    concept: Added to stack concept and pratice problem

--- dsa/Stack/print_&_reverse_stack.cpp
+++ dsa/Stack/print_&_reverse_stack.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
+      int n;
+      cin >> n;
+      stack<int> st;
+
+      for (int i = 0; i < n; i++)
+      {
+            int x;
+            cin >> x;
+      }
 
       return 0;
 }

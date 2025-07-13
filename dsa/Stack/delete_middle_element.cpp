a3772750f5d1e47be746f70c5bb5ffdcce47fbb4
mamun2519 <juborajislam46@gmail.com>
2025-07-13 16:21:46 +0600

    concept: Added to stack concept and pratice problem

--- dsa/Stack/delete_middle_element.cpp
+++ dsa/Stack/delete_middle_element.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void deleteMiddle(stack<int> &st, int current, int middle)
+{
+      if (current == middle)
+      {
+            st.pop();
+            return;
+      }
+
+      int topElement = st.top();
+      st.pop();
+      // recersive
+      deleteMiddle(st, current + 1, middle);
+      st.push(topElement);
+}
 int main()
 {
       int n;
@@ -14,11 +28,16 @@ int main()
             st.push(x);
       }
 
-      int size = n - 1;
-      while (!st.empty())
-      {
-            /* code */
-      }
+      int size = st.size();
+      int middle = size / 2;
+      stack<int> st2;
+      deleteMiddle(st2, 0, middle);
 
+      while (!st2.empty())
+      {
+            cout << st.top() << " ";
+            st.pop();
+      }
+      cout << endl;
       return 0;
 }

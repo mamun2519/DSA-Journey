19ac09176c6d29b0cdb5a836c537f841c06d2b68
mamun2519 <juborajislam46@gmail.com>
2025-07-13 15:44:24 +0600

    concept: Added to stack concept and pratice problem

--- dsa/Stack/valid_parentheses.cpp
+++ dsa/Stack/valid_parentheses.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-
+      string s;
+      cin >> s;
+      stack<char> st;
+      for (char c : s)
+      {
+            if (c == '(' || c == '{' || c == '[')
+            {
+            }
+      }
       return 0;
 }

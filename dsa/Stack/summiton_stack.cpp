56f49e12c8ee80402a80bd2f950c7443616ebdd9
mamun2519 <juborajislam46@gmail.com>
2025-07-14 14:17:45 +0600

    Problem: added to stack reloaded propblen

--- dsa/Stack/summiton_stack.cpp
+++ dsa/Stack/summiton_stack.cpp
@@ -6,11 +6,22 @@ int main()
       int n;
       cin >> n;
       stack<int> st;
+      // take input
       for (int i = 0; i < n; i++)
       {
             int x;
             cin >> x;
             st.push(x);
       }
+
+      // sumation
+      int sum = 0;
+      while (!st.empty())
+      {
+            int topValue = st.top();
+            sum += topValue;
+            st.pop();
+      }
+
       return 0;
 }

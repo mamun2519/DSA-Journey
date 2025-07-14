9d2b7f5aa55e359b4d0ade4ccd0a1513ea7dc34e
mamun2519 <juborajislam46@gmail.com>
2025-07-14 14:16:01 +0600

    Problem: added to stack reloaded propblen

--- dsa/Stack/summiton_stack.cpp
+++ dsa/Stack/summiton_stack.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-
+      int n;
+      cin >> n;
+      stack<int> st;
+      for (int i = 0; i < n; i++)
+      {
+            int x;
+            cin >> x;
+            st.push(x);
+      }
       return 0;
 }

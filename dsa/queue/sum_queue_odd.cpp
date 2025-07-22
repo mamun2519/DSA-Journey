dd8b702631e9d67804ea8c5de9e8cf65cc41cfea
mamun2519 <juborajislam46@gmail.com>
2025-07-22 16:42:29 +0600

    feat: added to basic

---
+++ dsa/queue/sum_queue_odd.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int n;
+      cin >> n;
+      queue<int> q;
+      for (int i = 0; i < n; i++)
+      {
+            int x;
+            cin >> x;
+            q.push(x);
+      }
+
+      int odd = 0;
+
+      for (int i = 0; i < n; i++)
+      {
+
+            // cout << q[i];
+      }
+
+      return 0;
+}

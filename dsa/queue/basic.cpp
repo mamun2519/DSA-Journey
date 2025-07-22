3e01ca7067b3da830974e0e98c26f6f2109c47b5
mamun2519 <juborajislam46@gmail.com>
2025-07-22 16:27:00 +0600

    feat: added to basic

--- dsa/queue/basic.cpp
+++ dsa/queue/basic.cpp
@@ -12,5 +12,8 @@ int main()
       q.push(20); // 10 20
       q.push(30); // 10 20 30
 
-            return 0;
+      // print the font element
+      cout << q.front() << endl;
+
+      return 0;
 }

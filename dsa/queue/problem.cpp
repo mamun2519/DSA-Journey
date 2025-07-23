2073b8774ee8be47deaded4d285694feb3f21d68
mamun2519 <juborajislam46@gmail.com>
2025-07-23 22:57:33 +0600

    Leetcode: try to leetcode problem solve

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
+      int n;
+      cin >> n;
+      int arr[n];
+      for (int i = 0; i < n; i++)
+      {
+            cin >> arr[i];
+      }
+      int target;
+      cin >> target;
 
-      return 0;
+      stack<int> st;
+      for (int i = 0; i < n; i++)
+      {
+            if (arr[i] <= target)
+                  st.push(i);
+      }
+      else if () return 0;
 }

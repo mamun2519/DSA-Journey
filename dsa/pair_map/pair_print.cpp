7d1e06e31f37a94d59478c36cf178a29af5c1050
mamun2519 <juborajislam46@gmail.com>
2025-07-27 12:21:51 +0600

    map related concept and problem

--- dsa/pair_map/pair_print.cpp
+++ dsa/pair_map/pair_print.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
-
+      string name;
+      int age;
+      cin >> name >> age;
+      pair<string, int> person;
+      person = make_pair(name, age);
+      cout << "Age of " << person.first << ": " << person.second << endl;
       return 0;
 }

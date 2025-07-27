864d403b1c03d3b706dcaaa82f99f23a2a24edfb
mamun2519 <juborajislam46@gmail.com>
2025-07-27 12:06:44 +0600

    added to pair and related problem

--- dsa/pair_map/pair.cpp
+++ dsa/pair_map/pair.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main()
 {
 
+      pair<int, int> pr;
+      pr = make_pair(1, 2);
+      cout << pr.first << " " << pr.second << endl;
+
+      // nested pair
+      pair<string, pair<int, int>> nested_pr;
+
       return 0;
 }

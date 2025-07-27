a77e9e341b07a2d3c71c855f4e8d5addc5cee7c4
mamun2519 <juborajislam46@gmail.com>
2025-07-27 12:16:14 +0600

    map related concept and problem

--- dsa/pair_map/map.cpp
+++ dsa/pair_map/map.cpp
@@ -6,7 +6,12 @@ int main()
 {
       // declare a map
       // map<key_type, value_type> map_name;
-      map<string, int> name;
+      map<string, int> age;
+
+      // insert elements into the map
+      age["juboraj"] = 24;
+      age["john"] = 30;
+      age["alice"] = 22;
 
       return 0;
 }

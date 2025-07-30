914126eb1399dbae61bc18d2704f0aa4b2871d82
mamun2519 <juborajislam46@gmail.com>
2025-07-30 16:31:12 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 int main()
 {
-
       ios::sync_with_stdio(false);
       cin.tie(nullptr);
+
       int n;
-      cin >> n;
+      if (!(cin >> n))
+            return 0;
+
       while (n--)
       {
             string s;
-            cin >> s;
-            if (s.size() < 10)
-            {
-                  cout << s.front() << (s.size() - 2) << s.back() << endl;
+            cin >> s; // words are 1..100 lowercase letters
+            if (s.size() > 10)
+            { // strictly more than 10
+                  cout << s.front() << (s.size() - 2) << s.back() << '\n';
             }
             else
             {
-                  cout << s << endl;
+                  cout << s << '\n';
             }
       }
+      return 0;
 }

84b9ff22734a66c172fbdccd6b71bd97376a2af6
mamun2519 <juborajislam46@gmail.com>
2025-07-30 16:24:23 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,30 +3,4 @@ using namespace std;
 
 int main()
 {
-
-      int t;
-      cin >> t;
-      int n;
-      cin >> n;
-      int arr[n];
-
-      while (t--)
-      {
-            int maxValue = arr[0];
-            for (int i = 0; i < n; i++)
-            {
-                  cin >> arr[i];
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                  if (arr[i] > maxValue)
-                  {
-                        maxValue = arr[i];
-                  }
-            }
-            cout << maxValue;
-      }
-
-      return 0;
 }

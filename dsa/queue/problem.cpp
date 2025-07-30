c15fa2588eb4735021aad4f7adbd6b12544c1dd2
mamun2519 <juborajislam46@gmail.com>
2025-07-30 16:27:50 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -12,6 +12,11 @@ int main()
             cin >> s;
             if (s.size() < 10)
             {
+                  cout << s.front() << (s.size() - 2) << s.back() << endl;
+            }
+            else
+            {
+                  cout << s << endl;
             }
       }
 }

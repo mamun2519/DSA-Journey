9cea06ebf85cb4fe2f00bbb54f493843b9ac3571
mamun2519 <juborajislam46@gmail.com>
2025-07-30 16:41:29 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-
+      int y;
+      cin >> y; // Read the number of words
+      for (int year = y + 1;; y++)
+      {
+            string s;
+            cin >> s; // Read each word
+            set<char> st(s.begin(), s.end());
+            if ((int)st.size() == (int)s.size())
+            { // all digits are unique
+                  cout << year << '\n';
+                  break;
+            }
+      }
       return 0;
 }

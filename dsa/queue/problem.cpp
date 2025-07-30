9a342b8388e164c709bd2dd9ada1beb9077d1dd1
mamun2519 <juborajislam46@gmail.com>
2025-07-30 16:37:21 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,25 +3,6 @@ using namespace std;
 
 int main()
 {
-      ios::sync_with_stdio(false);
-      cin.tie(nullptr);
 
-      int n;
-      if (!(cin >> n))
-            return 0;
-
-      while (n--)
-      {
-            string s;
-            cin >> s;
-            if (s.size() > 10)
-            {
-                  cout << s.front() << (s.size() - 2) << s.back() << '\n';
-            }
-            else
-            {
-                  cout << s << '\n';
-            }
-      }
       return 0;
 }

499935130cf3bae4066ab007fa8d88ddaa1a9935
mamun2519 <juborajislam46@gmail.com>
2025-07-30 19:48:39 +0600

    Problem Try to solve codeForces problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,34 +3,4 @@ using namespace std;
 
 int main()
 {
-      int t;
-      cin >> t;
-      while (t--)
-      {
-            int n;
-            cin >> n;
-            int d[n];
-            for (int i = 0; i < n; i++)
-            {
-                  cin >> d[i];
-            }
-            // your code goes here
-            bool isSorted = true;
-            for (int i = 0; i < n - 1; i++)
-            {
-                  if (d[i] > d[i + 1])
-                  {
-                        isSorted = false;
-                        break;
-                  }
-            }
-            if (isSorted)
-            {
-                  cout << "Yes" << endl;
-            }
-            else
-            {
-                  cout << "No" << endl;
-            }
-      }
 }

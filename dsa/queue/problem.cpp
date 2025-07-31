6e7cc5cf9f483860a3cb34a44007b6be6442d1b2
mamun2519 <juborajislam46@gmail.com>
2025-07-31 12:37:12 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -3,31 +3,14 @@ using namespace std;
 
 int main()
 {
-      vector<int> v;
-      v = {2, 7, 11, 15};
-      int target = 26;
-
-      vector<int> ans;
-      for (int i = 0; i < v.size(); i++)
+      int t;
+      cin >> t;
+      while (t--)
       {
-            for (int j = i + 1; j < v.size(); j++)
-            {
-                  if (v[i] + v[j] == target)
-                  {
-                        ans.push_back(i);
-                        ans.push_back(j);
-                        break;
-                  }
-            }
-            if (!ans.empty())
-                  break;
-      }
-      if (ans.size())
-      {
-            cout << "Indices: " << ans[0] << ", " << ans[1] << endl;
-      }
-      else
-      {
-            cout << "No pair found" << endl;
+            int n;
+            cin >> n;
+            string s;
+            cin >> s;
+            // your code goes here
       }
 }

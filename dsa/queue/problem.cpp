4743fe30bdcc6d9b362a7a17e13f2edebaf3d202
mamun2519 <juborajislam46@gmail.com>
2025-07-31 16:51:25 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main()
 {
 
-            return 0;
+      vector<int> arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+      int n = arr.size();
+      int k = 1;
+      for (int i = 1; i < n; i++)
+      {
+            if (arr[i] == arr[i + 1])
+            {
+                  k++;
+            }
+      }
+      cout << k << endl;
+      return 0;
 }

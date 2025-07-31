f0afeab8e4555ab40106b35a646c8feaa6c0000f
mamun2519 <juborajislam46@gmail.com>
2025-07-31 16:40:25 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -4,15 +4,5 @@ using namespace std;
 int main()
 {
 
-      int w;
-      cin >> w;
-      if (w % 2 == 0 && w >= 4)
-      {
-            cout << "YES" << endl;
-      }
-      else
-      {
-            cout << "NO" << endl;
-      }
-      return 0;
+            return 0;
 }

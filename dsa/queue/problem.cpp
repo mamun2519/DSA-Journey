282b9f304b4274be609270557d0429401bd517e6
mamun2519 <juborajislam46@gmail.com>
2025-07-31 13:04:00 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -7,11 +7,24 @@ int main()
       cin >> T;
       while (T--)
       {
-            int n;
-            cin >> n;
+
             string s;
             cin >> s;
+
+            string titleCase = "";
+            for (int i = 0; i < s.size(); i++)
+            {
+                  if (i == 0 || s[i - 1] == ' ')
+                  {
+                        titleCase += toupper(s[i]);
+                  }
+                  else
+                  {
+                        titleCase += tolower(s[i]);
+                  }
             }
+            cout << titleCase << endl;
+      }
 
       return 0;
 }

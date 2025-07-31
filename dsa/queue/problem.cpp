111210ebbbaf229d0ee6e60a7fbb27e55f1f5c76
mamun2519 <juborajislam46@gmail.com>
2025-07-31 13:00:08 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -11,17 +11,7 @@ int main()
             cin >> n;
             string s;
             cin >> s;
-            int insertions = 0;
-            for (int i = 0; i < n; i++)
-            {
-                  if (s[i] == s[i + 1])
-                  {
-                        insertions++;
-                  }
             }
-
-            cout << insertions << endl;
-      }
 
       return 0;
 }

5f3590c1932d5af65ba8ab91201a9bb56b039a80
mamun2519 <juborajislam46@gmail.com>
2025-07-31 16:25:31 +0600

    added to condechef problem

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -4,30 +4,7 @@ using namespace std;
 int main()
 {
 
-      int n;
-      cin >> n;
-
-      for (int i = 1; i <= n; i++)
-      {
-            if (i % 2 == 1)
-            {
-                  cout << "I hate";
-            }
-            else
-            {
-                  cout << "I love";
-            }
-
-            if (i == n)
-            {
-                  cout << " it";
-            }
-            else
-            {
-                  cout << " that ";
-            }
-      }
-      cout << endl;
-
+      int w;
+      cin >> w;
       return 0;
 }

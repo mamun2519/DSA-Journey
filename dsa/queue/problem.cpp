2bbde653053f24c474a2b42e3a40b57c7632a830
mamun2519 <juborajislam46@gmail.com>
2025-08-03 22:51:44 +0600

    Problem: try to solve stack related problem in the codeshef

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+// g++ insert_array.cpp -o test.exe && ./test.exe < input.txt > output.txt
 
 int main()
 {
 
-      vector<int> arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-      int n = arr.size();
-      int k = 1;
-      for (int i = 1; i < n; i++)
-      {
-            if (arr[i] == arr[i + 1])
-            {
-                  k++;
-            }
-      }
-      cout << k << endl;
-      return 0;
+            return 0;
 }

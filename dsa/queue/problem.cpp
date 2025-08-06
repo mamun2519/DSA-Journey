26efa5d350748f5c24fe19c8ab5c89eb3cec4bf4
mamun2519 <juborajislam46@gmail.com>
2025-08-06 21:44:46 +0600

    codechep problem solve

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -8,35 +8,7 @@ int main()
       cin >> t;
       while (t--)
       {
-            int n;
-            cin >> n;
-            int arr[n];
-            for (int i = 0; i < n; i++)
-            {
-                  cin >> arr[i];
-            }
-
-            int min_cost = 1000000000;
-            for (int i = 0; i < n; i++)
-            {
-                  for (int j = 0; j < n; j++)
-                  {
-                        if (i == j)
-                              continue;
-
-                        int cost;
-                        if (i + 1 == j)
-                        {
-                              cost = arr[i] + arr[j] / 2;
-                        }
-                        else
-                        {
-                              cost = arr[i] + arr[j];
-                        }
-                        min_cost = min(min_cost, cost);
-                  }
-            }
-            cout << min_cost << endl;
+            int a1, b1, a2, b2;
       }
       return 0;
 }

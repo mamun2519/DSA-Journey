4c7dfaa998a659224dacce959e753b48dd4087aa
mamun2519 <juborajislam46@gmail.com>
2025-08-06 21:25:37 +0600

    codechep problem solve

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -15,6 +15,14 @@ int main()
             {
                   cin >> arr[i];
             }
+
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                  for (int j = i + 1; j < n; j++)
+                  {
+                  }
+            }
       }
       return 0;
 }

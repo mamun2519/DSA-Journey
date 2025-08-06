9650e28312cb913d5626725bc43429d9e1b955e4
mamun2519 <juborajislam46@gmail.com>
2025-08-06 21:22:33 +0600

    codechep problem solve

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-
+      int t;
+      cin >> t;
+      while (t--)
+      {
+            int n;
+            cin >> n;
+            for (int i = 0; i < n; i++)
+            {
+            }
+      }
       return 0;
 }

09eea51673da37e2f1d47925cd84e44d9cb3a3ca
mamun2519 <juborajislam46@gmail.com>
2025-08-06 21:46:14 +0600

    codechep problem solve

--- dsa/queue/problem.cpp
+++ dsa/queue/problem.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 // g++ insert_array.cpp -o test.exe && ./test.exe < input.txt > output.txt
-
+bool isPossible(int a1, int b1, int a2, int b2)
+{
+      // Check if the first pair can be transformed into the second pair
+      if (a1 == a2 && b1 == b2)
+      {
+            return true;
+      }
+      // Check if the first pair can be transformed into the second pair by swapping
+      if (a1 == b2 && b1 == a2)
+      {
+            return true;
+      }
+      // Check if the first pair can be transformed into the second pair by adding or subtracting
+      if (abs(a1 - a2) == abs(b1 - b2))
+      {
+            return true;
+      }
+      return false;
+}
 int main()
 {
       int t;
